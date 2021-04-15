0feb4912275b882412613e5dcd8728ca6ab72f89
Nikita Manchuk <38407912+klorman@users.noreply.github.com>
2021-04-15 18:44:21 +0300

    Delete Light.hpp

--- Light.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Sphere.hpp"
-
-class Light : public Sphere { //этот класс уже не нужен
-public:
-    double power_;
-
-    Light(const Vector& center, const Vector& color, double radius, double power);
-};
