82b5d09f0d247fba656715b1a2cc5ac5bba791b6
Nikita Manchuk <38407912+klorman@users.noreply.github.com>
2021-04-15 18:44:13 +0300

    Delete Light.cpp

--- Light.cpp
+++
@@ -1,6 +0,0 @@
-#include "Light.hpp"
-
-Light::Light(const Vector& center, const Vector& color, double radius, double power) :
-    Sphere(center, color, radius, { 0, 0, -1, 0 }),
-    power_(power)
-{}
