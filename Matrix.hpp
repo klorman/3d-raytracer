0b6486004d9062183e4dd49730a0862cae83fd6c
Nikita Manchuk <38407912+klorman@users.noreply.github.com>
2021-04-17 00:37:27 +0300

    Рефакторинг

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,21 +1,105 @@
-#include "Vector.hpp"
+#pragma once
 
-struct RECT {
-    int row1, col1, row2, col2;
+#include <cassert>
+
+template <int rows, int cols>
+class matrix {
+public:
+    double data_[rows][cols];
+    int row_, col_;
+
+    matrix(const matrix<rows, cols>& m) :
+        data_(m.data_),
+        row_ (rows),
+        col_ (cols)
+    {}
+
+    matrix(double** data) :
+        data_(data)
+    {}
+
+    ~matrix() {
+        delete[] data_;
+    }
+
+    matrix<rows, cols>& operator= (const matrix<rows, cols>& m); //всё переделать
+    matrix<rows, cols>& operator[](int row); //всё переделать
+    matrix<rows, cols>  operator- (const matrix<rows, cols>& m) const;  //всё переделать
+    matrix<rows, cols>  operator+ (const matrix<rows, cols>& m) const;  //всё переделать
+    matrix<rows, cols>  operator* (const matrix<rows, cols>& m) const;  //всё переделать
 };
 
-struct mat4 {
-    const int SIZE = 4;
+template <int rows, int cols>
+matrix<rows, cols>& matrix<rows, cols>::operator=(const matrix<rows, cols>& m) {
+    if (this == &m) {
+		return *this;
+	}
 
-    double** data_;
+    if (col_ < m.col_ || row_ < m.row_) {
+        col_ = m.col_;
+        row_ = m.row_;
+        data_ = m.data_;
 
-    mat4();
-    mat4(const mat4& m);
-    ~mat4();
+        return *this;
+    }
 
-    mat4&   operator =  (const mat4& m);
-    double* operator [] (int row);
-    mat4    operator -  (const mat4& m) const;
-    mat4    operator +  (const mat4& m) const;
-    mat4    operator *  (const mat4& m) const;
-};
+    for(int i = 0; i < m.row_; i++) {
+        for(int j = 0; j < m.col_; j++) {
+            data_[i][j] = m.data_[i][j];
+        }
+    }
+
+    return *this;
+}
+
+template <int rows, int cols>
+matrix<rows, cols>& matrix<rows, cols>::operator[](int row) {
+    return data_[row];
+}
+
+template <int rows, int cols>
+matrix<rows, cols> matrix<rows, cols>::operator-(const matrix<rows, cols>& m) const {
+    assert(col_ == m.col_ && row_ == m.row_);
+
+    matrix<rows, cols> res;
+
+    for (int i = 0; i < res.row_; i++) {
+        for (int j = 0; j < res.col_; j++) {
+            res[i][j] = data_[i][j] - m.data_[i][j];
+        }
+    }
+
+    return res;
+}
+
+template <int rows, int cols>
+matrix<rows, cols> matrix<rows, cols>::operator+(const matrix<rows, cols>& m) const {
+    assert(col_ == m.col_ && row_ == m.row_);
+
+    matrix<rows, cols> res(m.col_, m.row_);
+
+    for (int i = 0; i < res.row_; i++) {
+        for (int j = 0; j < res.col_; j++) {
+            res[i][j] = data_[i][j] + m.data_[i][j];
+        }
+    }
+
+    return res;
+}
+
+template <int rows1, int cols1, int rows2, int cols2>
+matrix<rows1, cols2> matrix<rows1, cols1>::operator*(const matrix<rows2, cols2>& m) const {
+    assert(col_ == m.row_);
+
+    matrix<(rows1, cols2> res;
+    
+    for (int i = 0; i < row_; i++) {
+        for (int j = 0; j < m.col_; j++) {
+            for (int elem = 0; elem < col_; elem++) {
+                res.data_[i][j] += data_[i][elem] * m.data_[elem][j];
+            }
+        }
+    }
+
+    return res;
+}
